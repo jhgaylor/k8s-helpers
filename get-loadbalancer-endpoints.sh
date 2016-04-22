d05651e79d50429b94c20463314b4c5709003fae
Jake Gaylor <jgaylor@fsw.com>
2016-04-21 20:04:49 -0600

    ignore services that aren't load balanced

--- get-loadbalancer-endpoints.sh
+++ get-loadbalancer-endpoints.sh
@@ -1,1 +1,2 @@
-kubectl get svc | sed '1 s/^.*$//; /^$/d' | awk '{print $1}' | xargs kubectl describe svc | grep '^Name:\|^LoadBalancer Ingress:'
+#!/bin/bash
+kubectl get svc | grep -v '<none>' | sed '1 s/^.*$//; /^$/d' | awk '{print $1}' | xargs kubectl describe svc | grep '^Name:\|^LoadBalancer Ingress:'
